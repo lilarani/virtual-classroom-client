f867124b9c72ff0b578ce6644380c807acb61949
neelarani <neela9624@gmail.com>
2025-03-10 21:44:42 +0600

    Add Firebase configuration to .env file

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals, { node } from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
   { ignores: ['dist'] },
@@ -9,6 +9,7 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      node: true,
       globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
@@ -30,4 +31,4 @@ export default [
       ],
     },
   },
-]
+];
